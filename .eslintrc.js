5dcd13c5707cb9550ab7486ea27aeb16ab3ff1bb
Ebamba Kambwa Gédéon <gedebamba3@gmail.com>
2022-05-25 18:22:50 +0100

    feat: install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'react-native/react-native': true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-native',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': 0
+  },
+};
--- App.js
+++ App.js
@@ -1,21 +1,27 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text> {process.env.APP_NAME}</Text>
-      <StatusBar style="auto" />
+      <Text>
+        {' '}
+        {process.env.APP_NAME}
+      </Text>
+
+      <StatusBar />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
