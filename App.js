4f00dc01dad0be6d07f42d36d1073894a9414848
emmie125 <mulangaemmanuella@gmail.com>
2022-05-26 17:27:02 +0200

    feat: add a configuration navigation

--- App.js
+++ App.js
@@ -1,28 +1,46 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/react-in-jsx-scope */
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from './app/pages/home/Home'
 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     backgroundColor: '#fff',
     flex: 1,
+    color: 'red',
     justifyContent: 'center',
   },
-});
+})
 
-export default function App() {
+const Stack = createNativeStackNavigator()
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>
-        {' '}
-        {process.env.APP_NAME}
-      </Text>
-
-      <StatusBar />
-    </View>
-
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={({ navigation, route })=>({
+            title: 'My home',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+              alignItems: 'center',
+              justifyContent: 'center',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
+
+export default App
