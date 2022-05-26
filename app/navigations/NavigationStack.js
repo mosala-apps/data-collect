4feddc5db6131034be5fa3d49556437dbe995f78
rkota(Rosaire Kota) <rosairekota@gmail.com>
2022-05-26 18:48:26 +0100

    feat: add axios

--- app/navigations/NavigationStack.js
+++ app/navigations/NavigationStack.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import { createNativeStackNavigator} from "@react-navigation/native-stack"
-import HomeScreen from '../pages/home/Home'
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/home/Home';
 
-const Stack = createNativeStackNavigator ()
+const Stack = createNativeStackNavigator();
 
-export const NavigationStack = () => {
+export function NavigationStack() {
   return (
     <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={({ navigation, route })=>({
-            title: 'My home',
-            headerStyle: {
-              backgroundColor: '#f4511e',
-              alignItems: 'center',
-              justifyContent: 'center',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-          })}
-        />
-      </Stack.Navigator>
-  )
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={({ navigation, route }) => ({
+          title: 'My home',
+          headerStyle: {
+            backgroundColor: '#f4511e',
+            alignItems: 'center',
+            justifyContent: 'center',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        })}
+      />
+    </Stack.Navigator>
+  );
 }
-
--- app/utils/README.md
+++
@@ -1,1 +0,0 @@
-# The utilities functions directories
