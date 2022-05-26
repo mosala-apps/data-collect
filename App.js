a5f57df758d03ea48931a92e556d033b7a3f754b
rkota(Rosaire Kota) <rosairekota@gmail.com>
2022-05-26 17:29:06 +0100

    refactor: change RootNavigation file

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import RootNavigation from "./routes"
 import { Provider } from 'react-redux';
+import RootNavigation from './app/components/navigations/RootNavigation';
 import { store } from './app/store';
 
 export default function App() {
