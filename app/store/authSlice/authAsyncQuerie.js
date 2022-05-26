6c0d1d8c0ddbf6a241361b3d60036babe9ff4b22
rkota(Rosaire Kota) <rosairekota@gmail.com>
2022-05-26 16:15:08 +0100

    add authSilce in the store

---
+++ app/store/authSlice/authAsyncQuerie.js
@@ -0,0 +1,8 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+export const login = createAsyncThunk(
+  'user/login',
+  async (payload, thunkAPI) => {
+    
+  },
+);
--- app/store/store.js
+++ app/store/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import authSlice from "./authSilce/authSlice"
 export const store = configureStore({
   reducer: {
-
+    auth: authSlice
   },
 });
