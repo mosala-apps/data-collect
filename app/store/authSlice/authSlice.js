e6d4cc9349f185a1647bc9ba402c5c8a20fe1ad3
rkota(Rosaire Kota) <rosairekota@gmail.com>
2022-05-26 18:21:48 +0100

    fix: resolve redux issue

--- app/store/authSlice/authSlice.js
+++ app/store/authSlice/authSlice.js
@@ -1,37 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { login} from "./authAsyncQuerie"
-const initialState = {
-  user: {},
-  isLoading: false,
-  isAuthenticated: false,
-  isLogout: false,
-  authError: false,
-  userRoles: [],
-  lastDateAuth: null,
-}
+import { login } from './authAsyncQuerie';
 
-const authSlice = createSlice({
+const AuthSlice = createSlice({
   name: 'user',
-  initialState,
+  initialState: {
+    user: {},
+    isLoading: false,
+    isAuthenticated: false,
+    isLogout: false,
+    authError: false,
+    userRoles: [],
+    lastDateAuth: null,
+  },
   reducers: {},
-  extraReducers:(builder)=>{
-    builder.addCase(login.pending, (state)=>{
-      state.isLoading = true
-    }),
-    builder.addCase(login.fulfilled, (state, {payload})=>{
-      state.isLoading = false
-      state.isAuthenticated = true
-      state.authError = false
-      state.lastDateAuth = new Date()
-      state.user = payload.user
-    }),
-    builder.addCase(login.rejected, (state)=>{
-      state.isLoading = false
-      state.authError = true
-    })
-
-  }
+  extraReducers: {
+    [login.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [login.fulfilled]: (state, { payload }) => {
+      state.isLoading = false;
+      state.isAuthenticated = true;
+      state.authError = false;
+      state.lastDateAuth = new Date();
+      state.user = payload.user;
+    },
+    [login.rejected]: (state) => {
+      state.isLoading = false;
+      state.authError = true;
+    },
+  },
 });
 
-const { reducer, actions} = authSlice
-export default reducer
+export default AuthSlice.reducer;
--- app/store/store.js
+++ app/store/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authSlice from "./authSilce/authSlice"
+import AuthSlice from './authSlice/authSlice';
 export const store = configureStore({
   reducer: {
-    auth: authSlice
+    auth: AuthSlice
   },
 });
