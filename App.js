d80492dd8360664906efdaba95c1989b383741b0
rkota(Rosaire Kota) <rosairekota@gmail.com>
2022-05-26 16:19:21 +0100

    configure the store in App.js

--- App.js
+++ App.js
@@ -2,6 +2,8 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,15 +16,17 @@ const styles = StyleSheet.create({
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>
-        {' '}
-        {process.env.APP_NAME}
-      </Text>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>
+          {' '}
+          {process.env.APP_NAME}
+        </Text>
 
-      <StatusBar />
-    </View>
+        <StatusBar />
+      </View>
+    </Provider>
 
   );
 }
