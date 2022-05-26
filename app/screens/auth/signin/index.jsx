843445e8a8adb6c6fea6de84c67b7e25ac318107
rkota(Rosaire Kota) <rosairekota@gmail.com>
2022-05-26 21:10:26 +0100

    style: add react-native-svg react-native-shadow-2

--- app/screens/auth/signin/index.jsx
+++ app/screens/auth/signin/index.jsx
@@ -1,16 +1,42 @@
-import { Text, View } from 'react-native';
+import { Text, View,TextInput,Pressable} from 'react-native';
 import React from 'react';
 import { styles } from './signin.style';
+import {Shadow} from 'react-native-shadow-2';
+
 
 const Signin =({ navigation }) =>{
   return (
     <View style={styles.signin__container}>
-     <View style={styles.signin__form}>
-       <View>
-       <Text>Connexion</Text>
-       <Text>Entrez vos paramètres de connexion pour continuer</Text>
+      <Shadow 
+       distance={5}
+       startColor={'#00000010'}
+       containerViewStyle={{marginVertical: 20}}
+       radius={8}
+       style={styles.signin__form}
+      >
+       <View style={styles.signin__form_header}>
+       <Text style ={styles.signin__form_title}>Connexion</Text>
+       <Text style ={styles.signin__form_text}>
+         Entrez vos paramètres de connexion pour continuer
+         </Text>
        </View>
-     </View>
+       <View style={styles.signin__form_body}>
+        <View style={styles.signin__form_group}>
+          <Text>EMAIL, UTILISATEUR OU  TELEPHONE</Text>
+          <TextInput style={styles.signin__form_input}/>
+        </View>
+        <View style={styles.signin__form_group}>
+          <View style={styles.signin__form_label}>
+          <Text>MOT DE PASSE  Mot de passe oublié ?</Text>
+          </View>
+          <TextInput style={styles.signin__form_input}/>
+        </View>
+        <Pressable style={styles.signin__form_button}>
+         <Text style={{textAlign:'center', color: '#ffff'}}>Connexion</Text>
+        </Pressable>
+       </View>
+      </Shadow>
+     
     </View>
   );
 }
--- app/screens/auth/signin/signin.style.js
+++ app/screens/auth/signin/signin.style.js
@@ -1,21 +1,60 @@
 import { StyleSheet } from 'react-native';
 import variableStyle from '../../../config/variable.style';
-export const styles = StyleSheet.create({
-    signin__container:{
-        flex: 1,
-        alignItems:'center',
-        justifyContent:'center',
-        backgroundColor:variableStyle.secondaryColor,
-        height: '100%',
-        paddingLeft:'5%',
-        paddingRight: '5%'
-    },
 
-    signin__form:{
-        backgroundColor:'#fff',
-        shadow: variableStyle.boxWithShadow,
-        height: '50%',
-        width: '100%',
-        borderRadius:10,
-    }
+export const styles = StyleSheet.create({
+  signin__container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: variableStyle.primaryColor,
+    height: '100%',
+    paddingLeft: '2%',
+    paddingRight: '2%',
+  },
+
+  signin__form: {
+    backgroundColor: '#fff',
+    shadow: variableStyle.boxWithShadow,
+    height: '50%',
+    width: '100%',
+    borderRadius: 10,
+
+  },
+  signin__form_header: {
+    marginTop: '5%',
+    paddingLeft:'5%',
+    paddingRight:'5%'
+  },
+  signin__form_title: {
+    textAlign: 'center',
+    fontSize: 20,
+    color: '#14244F',
+
+  },
+  signin__form_text: {
+    textAlign: 'center',
+    fontSize: 12,
+    color: '#14244F',
+
+  },
+  signin__form_body: {
+    marginTop: '5%',
+    padding: '5%',
+  },
+  signin__form_input: {
+    borderWidth: 0.5,
+    borderColor: 'rgba(99, 132, 234, 0.48)',
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  signin__form_label: {
+    marginTop: 10,
+  },
+  signin__form_button: {
+      marginTop: 20,
+    width: '100%',
+    height: 31,
+    backgroundColor: variableStyle.secondaryColor,
+    borderRadius: 5,
+  },
 });
