3f7392bc15385c386aea7028ef5ffe866b0123a3
rkota(Rosaire Kota) <rosairekota@gmail.com>
2022-05-27 10:03:34 +0100

    feat: add secureTextEntry for the password

--- app/screens/auth/signin/index.jsx
+++ app/screens/auth/signin/index.jsx
@@ -1,48 +1,58 @@
-import { Image, Text, View,TextInput,Pressable,ToastAndroid} from 'react-native';
-import React from 'react';
+import {
+  Image, Text, View, TextInput, Pressable, ToastAndroid,
+} from 'react-native';
+import React, { useState } from 'react';
+import { Shadow } from 'react-native-shadow-2';
 import { styles } from './signin.style';
-import {Shadow} from 'react-native-shadow-2';
-import logo from '../../../../assets/img/logo_parteners.png'; 
+import logo from '../../../../assets/img/logo_parteners.png';
 
-const Signin =({ navigation }) =>{
-  const handleSignin =(e)=>{
-    e.preventDefault()
-    ToastAndroid.show("Veuillez vous authentifier!", ToastAndroid.SHORT, ToastAndroid.TOP);
+function Signin({ navigation }) {
+  const [isPasswordReveal, setIsPasswordReveal] = useState(false);
+  const handleRevealPassword =()=>{
+    setIsPasswordReveal(true)
   }
+  const handleSignin = (e) => {
+    e.preventDefault();
+    navigation.navigate('Home');
+  };
   return (
     <View style={styles.signin__container}>
-       <Image source={logo} style={styles.signin__logo} /> 
-      <Shadow 
-       distance={5}
-       startColor={'#00000010'}
-       containerViewStyle={{marginVertical: 20}}
-       radius={8}
-       style={styles.signin__form}
+      <Image source={logo} style={styles.signin__logo} />
+      <Shadow
+        distance={5}
+        startColor="#00000010"
+        containerViewStyle={{ marginVertical: 20 }}
+        radius={8}
+        style={styles.signin__form}
       >
-       <View style={styles.signin__form_header}>
-       <Text style ={styles.signin__form_title}>Connexion</Text>
-       <Text style ={styles.signin__form_text}>
-         Entrez vos paramètres de connexion pour continuer
-         </Text>
-       </View>
-       <View style={styles.signin__form_body}>
-        <View style={styles.signin__form_group}>
-          <Text>EMAIL, UTILISATEUR OU  TELEPHONE</Text>
-          <TextInput style={styles.signin__form_input}/>
+        <View style={styles.signin__form_header}>
+          <Text style={styles.signin__form_title}>Connexion</Text>
+          <Text style={styles.signin__form_text}>
+            Entrez vos paramètres de connexion pour continuer
+          </Text>
         </View>
-        <View style={styles.signin__form_group}>
-          <View style={styles.signin__form_label}>
-          <Text>MOT DE PASSE</Text>
-          <Text style={styles.signin__forgotPassword}>Mot de passe oublié ?</Text>
+        <View style={styles.signin__form_body}>
+          <View style={styles.signin__form_group}>
+            <Text>EMAIL, UTILISATEUR OU  TELEPHONE</Text>
+            <TextInput style={styles.signin__form_input} />
           </View>
-          <TextInput style={styles.signin__form_input}/>
+          <View style={styles.signin__form_group}>
+            <View style={styles.signin__form_label}>
+              <Text>MOT DE PASSE</Text>
+              <Text style={styles.signin__forgotPassword}>Mot de passe oublié ?</Text>
+            </View>
+            <TextInput
+              style={styles.signin__form_input}
+              secureTextEntry={isPasswordReveal}
+              onFocus={handleRevealPassword}
+            />
+          </View>
+          <Pressable style={styles.signin__form_button} onPress={handleSignin}>
+            <Text style={styles.signin__button_text}>Connexion</Text>
+          </Pressable>
         </View>
-        <Pressable style={styles.signin__form_button} onPress={handleSignin}>
-         <Text style={styles.signin__button_text}>Connexion</Text>
-        </Pressable>
-       </View>
       </Shadow>
-     
+
     </View>
   );
 }
