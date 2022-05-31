c5807f5d621b3d2c0cfa99798d8509d66ef82aac
emmie125 <mulangaemmanuella@gmail.com>
2022-05-31 12:48:35 +0200

    feat: add a structure of home page

--- app/screens/home/index.jsx
+++ app/screens/home/index.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import HeaderNavigation from '../../navigations/headerNavigation'
-import styleSheet from './index.style'
+import React from 'react';
+import { Feather } from '@expo/vector-icons';
+import { Text, View, TextInput } from 'react-native';
+import HeaderNavigation from '../../navigations/headerNavigation';
+import styleSheet from './index.style';
 
 function Home() {
+  const [textInput, setTextInput] = React.useState('');
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styleSheet.container}>
       <HeaderNavigation />
       <View style={styleSheet.containerHome}>
-        {/* <View style={styleSheet.containerHomeForm} /> */}
+        <View style={styleSheet.containerHomeSearch}>
+          <Feather name="search" size={24} color="black" style={styleSheet.containerHomeSearchIcon} />
+          <TextInput
+            editable
+            style={styleSheet.containerHomeSearchTextInput}
+            onChangeText={(text) => setTextInput(text)}
+            value={textInput}
+            placeholder="Rechercher par formulaire"
+          />
+        </View>
+        <View style={styleSheet.containerHomeForm}>
+          <Text style={styleSheet.containerHomeFormTitle}>Mes formulaires</Text>
+        </View>
       </View>
-      <Text>Home Screen</Text>
     </View>
-  )
+  );
 }
 
-export default Home
+export default Home;
