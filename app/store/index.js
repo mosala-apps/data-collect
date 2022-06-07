0855c0d9937b6ea1a55b1e89c5a3728d4544105d
gaelmap <gmapwata6@gmail.com>
2022-06-07 18:05:34 +0100

    fix:conflict

--- app/store/index.js
+++ app/store/index.js
@@ -2,7 +2,6 @@ export { store, persistor } from './store';
 export { login, logout } from './auth/authAsyncQuerie';
 export { authSelector } from './auth/authSelectors';
 export { setUser } from './auth/authSlice';
-export { getForms } from './form/formAsyncQuerie'
 export { notificationsHospital, getNotificationNotRead,setNotificationNotRead } from './notification/notificationAsyncQuerie'
 export { addHospitalManagerNames } from './hospitalManagerName/hospitalManagerNameAsyncQuerie'
 export { getHospital } from './hospital/hospitalAsyncQuerie'
