3c0b83d8611785939310c69dfd29d7a644a11bca
gaelmap <gmapwata6@gmail.com>
2022-07-07 15:08:08 +0100

    feat: create new version apk

--- app/config/apiURL.js
+++ app/config/apiURL.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 // import { API_URL } from '@env';
 
 export default axios.create({
-  // baseURL: 'https://dev-dashboard.stopcoronavirusrdc.info/api/dashboard',
+  baseURL: 'https://dev-dashboard.stopcoronavirusrdc.info/api/dashboard',
   // baseURL: 'http://192.168.57.94:8000/api/dashboard',
-  baseURL: 'http://192.168.165.168:8000/api/dashboard',
+  // baseURL: 'http://192.168.165.168:8000/api/dashboard',
   // timeout: 1000,
 });
